28b5407190aa6cc30f6e1ad554838f17a6e0f24d
Kyaw Min Oo <96671581+kyawmincs@users.noreply.github.com>
2026-06-18 20:39:47 -0700

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let n = nums.length;
+        let i;
+
+        for (i = 0; i < n; i++) {
+            if (nums[i] === val) {
+                // Find first non-instance of val and swap
+                for (let j = i + 1; j < n; j++) {
+                    if (nums[j] !== val) {
+                        // Swap 
+                        const temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        break;
+                    }
+                }
+                // If not found until the end of loop, return i as k 
+                if (nums[i] === val) {
+                    return i;
+                }
+            }
+        }
+
+        return i;
+    }
+}
